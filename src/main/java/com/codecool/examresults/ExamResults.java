9c092face1f3a857909e3b8add867c41c091181c
MiklosHorvath <horvathmikloswork@gmail.com>
2021-04-07 09:40:19 +0200

    prepare starter repo

--- src/main/java/com/codecool/examresults/ExamResults.java
+++ src/main/java/com/codecool/examresults/ExamResults.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+package com.codecool.examresults;
+
 import java.util.*;
 
 public class ExamResults {
--- src/test/java/com/codecool/examresults/ExamResultsTest.java
+++ src/test/java/com/codecool/examresults/ExamResultsTest.java
@@ -1,26 +1,32 @@
+package com.codecool.examresults;
+
+import cccr.CCCRTestExecutionListener;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith({CCCRTestExecutionListener.class})
 class ExamResultsTest {
 
     @Test
     void getStudentsNameOrderedByExamResultsAverageReturnsCorrectList(){
         List<String> expected = Arrays.asList(
-                "Farkas László",
-                "Végh János",
-                "Kis Ferenc",
-                "Kocsis Anna",
-                "dr. Nagy Ivett",
-                "Nagy Zoltán",
-                "Kis Péter",
-                "Kovács Lilla",
-                "Csillag István",
-                "Kertész József",
-                "Kovács Károly Péter");
+                "Gottfried Wilhelm Leibniz",
+                "Alan Turing",
+                "Isaac Newton",
+                "Marie Curie",
+                "Barbara Liskov",
+                "Albert Einstein",
+                "Nikola Tesla",
+                "Ada Lovelace",
+                "Robert C. Martin",
+                "Linus Torvalds",
+                "James Clerk Maxwell");
         List<String> results = ExamResults.getStudentsNameOrderedByExamResultsAverageFromFile("src/main/resources/results.txt");
         assertEquals(expected,results);
     }
